5a7af81a6b9de5d0cdf9420623bc1f73169c6e9f
班玉都 <banyudu@icloud.com>
2016-03-16 22:16:19 +0800

    add login storyboard

---
+++ DYCooperation/DYCooperation/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  DYCooperation
+//
+//  Created by 班玉都 on 16/3/10.
+//  Copyright © 2016年 micro robot. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
--- DYCooperation/DYCooperation/ViewController.swift
+++ DYCooperation/DYCooperation/ViewController.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        /*
+        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(5 * Double(NSEC_PER_SEC)))
+        dispatch_after(time, dispatch_get_main_queue(), {
+        
+            let loginStoryboard : UIStoryboard = UIStoryboard(name: "Login", bundle: nil)
+            let loginViewController : UIViewController = loginStoryboard.instantiateViewControllerWithIdentifier("LoginViewController")
+            let window : UIWindow = UIApplication.sharedApplication().delegate!.window!!
+            window.rootViewController = loginViewController
+            window.makeKeyAndVisible()
+        })
+        */
+        
     }
 
     override func didReceiveMemoryWarning() {
